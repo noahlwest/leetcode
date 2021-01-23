15cbbf9c0b28e031cb6c6aa3833436d780c760ec
noahwest <nolwest@ucsc.edu>
2021-01-22 22:32:59 -0800

    Optimize 1_Two_Sum
    Removed calls to enumerate(), replaced with one call to len()

--- python/1_Two_Sum.py
+++ python/1_Two_Sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for index1, num1 in enumerate(nums):
-            for index2, num2 in enumerate(nums):
+        length = len(nums)
+        for index1 in range(length):
+            for index2 in range(length):
                 if (index1 == index2):
                     pass
-                elif ((num1 + num2) == target):
+                elif ((nums[index1] + nums[index2]) == target):
                     return [index1, index2]
                 
         return []
