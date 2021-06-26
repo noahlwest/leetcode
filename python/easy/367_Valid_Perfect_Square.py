70126ded75d06bdfc2a70457d4ea64d2edb6e156
noahwest <nolwest@ucsc.edu>
2021-06-26 11:41:47 -0700

    Improved 367

--- python/easy/367_Valid_Perfect_Square.py
+++ python/easy/367_Valid_Perfect_Square.py
@@ -1,10 +1,17 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        for i in range(0, 46341):
-            if num == i*i:
+        left = 0
+        right = 46341
+        while left < right:
+            middle = (left + right) // 2
+            if num == middle*middle:
                 return True
+            elif num > middle*middle:
+                left = middle+1
+            else: #num < middle*middle:
+                right = middle
             
         return False
 
-#runtime: O(1) (still slow, though)
+#runtime: O(1) [has a constant cap on execution time, but it's faster than linear O(1)]
 #memory: O(1)
