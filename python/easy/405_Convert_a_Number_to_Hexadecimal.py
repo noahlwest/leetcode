56262c48bda8d2d6d813f55edf5d686da4cb68ab
noahwest <nolwest@ucsc.edu>
2021-06-26 21:11:26 -0700

    Added 405

---
+++ python/easy/405_Convert_a_Number_to_Hexadecimal.py
@@ -0,0 +1,22 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        
+        #max int = 4294967296
+        #normalize to unsigned
+        if num < 0:
+            num += 4294967296
+        
+        digits = "0123456789abcdef"
+        
+        ret = ""
+        
+        while num > 0:
+            rem = num % 16
+            num = (num - rem)//16
+            ret = digits[rem] + ret
+
+        return ret
+#runtime: O(log n)
+#memory: O(1)
