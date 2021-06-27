edc0e926ae8f4b859a937952d2ed222ebfc6e7ab
noahwest <nolwest@ucsc.edu>
2021-06-26 19:42:07 -0700

    Improved 389

--- python/easy/389_Find_the_Difference.py
+++ python/easy/389_Find_the_Difference.py
@@ -1,20 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        cache = {}
-        for char in s:
-            if char in cache:
-                cache[char] += 1
-            else:
-                cache[char] = 1
+        extra_char = 0
         
-        for char in t:
-            if char in cache:
-                if cache[char] == 0:
-                    return char
-                else:
-                    cache[char] -= 1
-            else:
-                return char
+        for i in range(0, len(s)):
+            extra_char -= ord(s[i])
+            extra_char += ord(t[i])
+        return chr(extra_char + ord(t[-1]))
 
-#runtime: O(2n) -> O(n)
-#memory: O(n)
+#runtime: O(n) leetcode runtimes aren't that much better than cache method. (36 min cache vs 32 min this method). Likely due to sample sizes
+#memory: O(1) memory usage also not that much better. (typically higher! on leetcode) Likely due to sample sizes
