1495ed015c7f9024171fad6e22b3db5f658c8393
noahwest <nolwest@ucsc.edu>
2021-07-01 19:36:46 -0700

    Optimized 453

--- python/easy/453_Minimum_Moves_to_Equal_Array_Elements.py
+++ python/easy/453_Minimum_Moves_to_Equal_Array_Elements.py
@@ -1,21 +1,14 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        #if (sum / length) == max: all elements are equal
-        arr_sum = sum(nums)
         length = len(nums)
-        arr_max = max(nums)
-        moves = 0
+        if length == 0:
+            return 0
+        arr_sum = sum(nums)
+        arr_min = min(nums)
         
-        while arr_sum / length != arr_max:
-            #print(arr_sum, length, arr_max)
-            if arr_sum / length > arr_max:
-                arr_max += 1
-                arr_sum += length-1
-            elif arr_sum / length < arr_max:
-                arr_sum += length-1
-            moves += 1
+        moves = arr_sum - (arr_min * length)
                 
         return moves    
 
-#runtime: O(n) due to sum(), but really scales with (max-min)
+#runtime: O(n)
 #memory: O(1)
