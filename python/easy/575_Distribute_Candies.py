4b068fa94bebba9d71754b72dd83da887df55990
noahwest <nolwest@ucsc.edu>
2021-07-04 13:39:57 -0700

    Made 575 more pythonic

--- python/easy/575_Distribute_Candies.py
+++ python/easy/575_Distribute_Candies.py
@@ -1,15 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        cache = set()
-        types = 0
-        for num in candyType:
-            if num not in cache:
-                cache.add(num)
-                types += 1
-                
-        n = len(candyType)
-        
-        return min(types, int(n/2))
+        return min(len(set(candyType)), len(candyType)//2)
 
 #runtime: O(n)
-#memory: O(n)
+#memory: O(1)? Maybe O(n) from set()
