b8b67da67222721bba0da26e1532b794ac214a88
noahwest <nolwest@ucsc.edu>
2021-07-04 12:23:32 -0700

    Updated 561

--- python/easy/561_Array_Partition_I.py
+++ python/easy/561_Array_Partition_I.py
@@ -2,11 +2,11 @@ class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         #sort the nums, then take them as pairs from 0
         nums.sort()
-        sum = 0
+        ret = 0
         for i in range(0, len(nums), 2):
-            sum += min(nums[i], nums[i+1])
+            ret += min(nums[i], nums[i+1])
             
-        return sum
+        return ret
 
 #runtime: O(n/2) -> O(n)
 #memory: O(1)
