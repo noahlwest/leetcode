cb7c777bd75757277ccebc43da71691ad9809446
noahwest <nolwest@ucsc.edu>
2021-07-07 09:21:28 -0700

    Removed extra sum() from 682

--- python/easy/682_Baseball_Game.py
+++ python/easy/682_Baseball_Game.py
@@ -1,18 +1,23 @@
 class Solution:
     def calPoints(self, ops: List[str]) -> int:
         stack = []
+        ret = 0
         
         for op in ops:
             if op == "+":
                 stack.append(stack[-1] + stack[-2])
+                ret += stack[-1]
             elif op == "D":
                 stack.append(2 * stack[-1])
+                ret += stack[-1]
             elif op == "C":
-                stack.pop()
+                ret -= stack.pop()
             else:
                 stack.append(int(op))
+                ret += stack[-1]
+            print(ret)
                 
-        return sum(stack)
+        return ret
 
 #runtime: O(n)
 #memory: O(n)
