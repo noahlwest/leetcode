f4548faaeefbfbfb4b6aaf9e5a86ab28265e87aa
noahwest <nolwest@ucsc.edu>
2021-07-17 14:36:23 -0700

    Added 12

---
+++ python/medium/12_Integer_to_Roman.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values =   [1000, 900,  500, 400,  100, 90,   50,  40,   10,  9,    5,   4,    1]
+        numerals = ["M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        ret = ""
+        
+        i = 0
+        while num > 0:
+            if num >= values[i]:
+                num -= values[i]
+                ret += numerals[i]
+            else:
+                i += 1
+                
+        return ret
+
+#runtime: O(n)
+#memory: O(1)
