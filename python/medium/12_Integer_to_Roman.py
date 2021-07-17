763ff40f2d5f1d4a43da3fba8c2241d9e8096a29
noahwest <nolwest@ucsc.edu>
2021-07-17 14:39:05 -0700

    Converted string concatenation to array append w/ join()

--- python/medium/12_Integer_to_Roman.py
+++ python/medium/12_Integer_to_Roman.py
@@ -2,17 +2,17 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         values =   [1000, 900,  500, 400,  100, 90,   50,  40,   10,  9,    5,   4,    1]
         numerals = ["M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        ret = ""
+        ret_arr = []
         
         i = 0
         while num > 0:
             if num >= values[i]:
                 num -= values[i]
-                ret += numerals[i]
+                ret_arr.append(numerals[i])
             else:
                 i += 1
                 
-        return ret
+        return "".join(ret_arr)
 
 #runtime: O(n)
 #memory: O(1)
